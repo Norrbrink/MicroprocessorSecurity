349e9a9f9acdb19517790b32fc16d3412ef04260
Alexander Norrbrink <92300082+Norrbrink@users.noreply.github.com>
2022-03-23 10:23:02 +0000

    Update ACD.s
    Multiple Sensors Simultaneously

--- ACD.s
+++ ACD.s
@@ -5,17 +5,58 @@ global  ADC_Setup, ADC_Read
 psect	adc_code, class=CODE
     
 ADC_Setup:
-	bsf	TRISA, PORTA_RA0_POSN, A  ; pin RA0==AN0 input
-	banksel ANCON0
-	bsf	ANSEL0	    ; set AN0 to analog
-	banksel 0
-	movlw   0x01	    ; select AN0 for measurement
-	movwf   ADCON0, A   ; and turn ADC on
-	movlw   0x20	    ; Select 4.096V positive reference
-	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
-	movlw   0xF6	    ; Right justified output
-	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
-	return
+;bsf TRISA, PORTA_RA0_POSN, A ; pin RA0==AN0 input
+movlw 0x01
+movwf TRISA, A
+banksel ANCON0
+bsf ANSEL0 ; set AN0 to analog
+banksel 0
+;movlw 0x01 ; select AN0 for measurement
+movlw 0x07 ; select AN0, AN1, AN2 for measurement
+movwf ADCON0, A ; and turn ADC on
+movlw 0x20 ; Select 4.096V positive reference
+movwf ADCON1, A ; 0V for -ve reference and -ve input
+movlw 0xF6 ; Right justified output
+movwf ADCON2, A ; Fosc/64 clock and acquisition times
+return
+
+
+
+ADC_Setup1:
+;bsf TRISA, PORTA_RA0_POSN, A ; pin RA0==AN0 input
+movlw 0x02 ;pin RA1==AN1
+movwf TRISA, A
+banksel ANCON0
+bsf ANSEL0 ; set AN0 to analog
+banksel 0
+
+
+
+movlw 0x02 ; select AN1 for measurement
+movwf ADCON0, A ; and turn ADC on
+movlw 0x20 ; Select 4.096V positive reference
+movwf ADCON1, A ; 0V for -ve reference and -ve input
+movlw 0xF6 ; Right justified output
+movwf ADCON2, A ; Fosc/64 clock and acquisition times
+return
+
+ADC_Setup2:
+;bsf TRISA, PORTA_RA0_POSN, A ; pin RA0==AN0 input
+movlw 0x04 ;pin RA2==AN2
+movwf TRISA, A
+banksel ANCON0
+bsf ANSEL0 ; set AN0 to analog
+banksel 0
+
+
+
+movlw 0x04 ; select AN2 for measurement
+movwf ADCON0, A ; and turn ADC on
+movlw 0x20 ; Select 4.096V positive reference
+movwf ADCON1, A ; 0V for -ve reference and -ve input
+movlw 0xF6 ; Right justified output
+movwf ADCON2, A ; Fosc/64 clock and acquisition times
+return
 
 ADC_Read:
 	bsf	GO	    ; Start conversion by setting GO bit in ADCON0
